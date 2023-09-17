91be34b4fcb9adc69c33c828c8706e9ceb98db2b
李寻 <lixun@lixundeMacBook-Pro.local>
2023-09-17 15:58:40 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,40 +4,38 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
+        int i = 1;
 
-        for (int i = 1; i < 100; i++) {
+        while (i <= 100) {
 
-            // Find out which numbers divide i.
-            BuzzFizz result = doBuzzFizz(i);
-
-            // Print our appropriate result.
-            if (result.divisibleBy3() && result.divisibleBy5()) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (result.divisibleBy3()) {
-
-                System.out.println("Fizz");
-
-            } else if (result.divisibleBy5()) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            i = doFizzBuzz(i);
         }
     }
 
-    public static BuzzFizz doBuzzFizz(int i) {
+    private static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
-        BuzzFizz result = new BuzzFizz(divisibleBy3, divisibleBy5);
-        return result;
-    }
 
-    private record BuzzFizz(boolean divisibleBy3, boolean divisibleBy5) {
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
